3ba6b5a0d63b380ae8f22333ef0fd244615faa09
6829nkhpas <parnamnkh1@gmail.com>
2025-09-24 19:11:04 +0530

    Add Apiresponse and ErrorResponse classes for standardized API responses

---
+++ src/utils/Apirespose.js
@@ -0,0 +1,10 @@
+class Apiresponse {
+    constructor(statusCode, message="Success", data) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+        this.success = statusCode >= 200 && statusCode < 300;
+    }
+}
+
+export default Apiresponse;
---
+++ src/utils/ErrorRespose.js
@@ -0,0 +1,19 @@
+class ErrorResponse extends Error {
+    constructor(statusCode, stack="", message="Error occured ", errors=[]) {
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = null;
+        this.errors = errors;
+        this.success = false;
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+
+
+export default ErrorResponse;
+        
