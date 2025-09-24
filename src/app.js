9fac09474157567e56c6ec2be331f2c1160dea22
6829nkhpas <parnamnkh1@gmail.com>
2025-09-24 18:01:05 +0530

    Add CORS support to the application and update dependencies

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import dotenv from 'dotenv';
 dotenv.config({
@@ -6,7 +7,19 @@ dotenv.config({
 });
 
 const app = express();
-
+//basic configuration
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static('public'));
+ 
+//cors configuration
+app.use(cors({
+    origin: process.env.origin?.split(",") || "http://localhost:3000",
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+//health check endpoint
 app.get('/health', (req, res) => {
     res.status(200).send('OK');
 });
