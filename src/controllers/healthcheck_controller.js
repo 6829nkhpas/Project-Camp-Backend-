7e289679434910a441cedbd29591ab948514ff32
6829nkhpas <parnamnkh1@gmail.com>
2025-09-25 02:23:34 +0530

    Refactor health check controller to use async handler for improved error handling

--- src/controllers/healthcheck_controller.js
+++ src/controllers/healthcheck_controller.js
@@ -1,15 +1,11 @@
 import Apiresponse from "../utils/Apirespose.js";
+import asynchandler from "../utils/asynchandler.js";
 
 
 
-const healthcheck = (req, res) => {
-    try {
-        const response = new Apiresponse(200, "Service is healthy");
-        res.status(200).json(response);
-    } catch (error) {
-        console.log("Error in healthcheck:", error);
-        
-        
-    }
-}
+const healthcheck = asynchandler(async (req, res) => {
+    const response = new Apiresponse(200, "Service is healthy");
+    res.status(200).json(response);
+});
+
 export default healthcheck;
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,7 @@
+const  asynchandler = (requestHandler) => {
+    return async (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+    }
+};
+
+export default asynchandler;
